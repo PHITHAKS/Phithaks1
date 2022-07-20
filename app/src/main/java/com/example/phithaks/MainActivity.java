1c52f7192c0c6f6c0482dbf21f4806c68b9c66c2
ronnakrit1322 <thitiyaporn>
2022-07-20 10:34:20 +0700

    Initial commit

---
+++ app/src/main/java/com/example/phithaks/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.phithaks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
